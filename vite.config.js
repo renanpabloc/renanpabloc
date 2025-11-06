c0d5821765146efe2e92ef2045b7f599959ad9d0
Gustavo Campelo <gustavocampelods@gmail.com>
2025-11-05 22:56:31 -0300

    feat: ajustes finais

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // plugin oficial do Tailwind para Vite
+    tailwindcss(),
   ],
   base: '/',
 })
